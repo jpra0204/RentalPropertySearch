1179e707f6f57b1661d020048df5e2b7c2eda9c9
jpra0204 <jp.roca.angulo@gmail.com>
2025-02-05 18:07:42 -0500

    creating the header image component on the single property page and taking info from the database

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { PropertyHeaderImage } from '@/components/PropertyHeaderImage'
+import connectDB from '@/config/database'
+import Property from '@/models/Property'
+import Link from 'next/link'
+import { FaArrowLeft } from 'react-icons/fa'
 
-function SinglePropertyPage() {
+const SinglePropertyPage = async ({ params }) => {
+
+  const { id } = await params;
+  await connectDB();
+  const property = await Property.findById(id).lean();
+
+
   return (
-    <div>SinglePropertyPage</div>
+    <>
+      <PropertyHeaderImage image={ property.images[0] } />
+
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
+        </div>
+      </section>
+
+      <section class="bg-blue-50">
+        <div class="container m-auto py-10 px-6">
+          <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"></div>
+        </div>
+      </section>
+    </>
   )
 }
 
