d458e873be066cf6fdf594c33e2bfcff35f2dce0
jpra0204 <jp.roca.angulo@gmail.com>
2025-02-10 14:37:49 -0500

    creating the PropertyDetails component and using it in the single property page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -4,6 +4,7 @@ import connectDB from '@/config/database'
 import Property from '@/models/Property'
 import Link from 'next/link'
 import { FaArrowLeft } from 'react-icons/fa'
+import PropertyDetails from '@/components/PropertyDetails'
 
 const SinglePropertyPage = async ({ params }) => {
 
@@ -27,9 +28,10 @@ const SinglePropertyPage = async ({ params }) => {
         </div>
       </section>
 
-      <section class="bg-blue-50">
-        <div class="container m-auto py-10 px-6">
-          <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"></div>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"></div>
+          <PropertyDetails property={ property } />
         </div>
       </section>
     </>
