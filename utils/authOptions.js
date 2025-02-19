fc9bf88b363a73ece53e3ad31a4c16e87e69fde1
jpra0204 <jp.roca.angulo@gmail.com>
2025-02-19 12:38:37 -0500

    correcting typos - user creation is fully functional

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google"
+import connectDB from "@/config/database"
+import User from "@/models/User"
 
 export const authOptions = {
     providers: [
@@ -16,17 +18,32 @@ export const authOptions = {
     ],
     callbacks: {
         // Called on successful sign in
-        async singIn({ profile }) {
+        async signIn({ profile }) {
             // Connect to databse
+            await connectDB()
             // Check if user exists
+            const userExists = await User.findOne({ email: profile.email })
             // if not, create user
+            if (!userExists) {
+                // Limit the name to 20 characters
+                const userName = profile.name.slice(0, 20)
+                await User.create({
+                    email: profile.email,
+                    username: userName,
+                    image: profile.image
+                })
+            }
             // return true to allow to sign in
+            return true;
         },
         // Session callback that modifies the session object
         async session({ session }) {
             // Get user from databse
+            const user = await User.findOne({ email: session.user.email })
             // Assign user id from the session
+            session.user.id = user._id.toString();
             // Return the session
+            return session;
         }
     }
 }
