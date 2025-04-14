04f17f5050b95ddb795d2acd6ca9d458931524da
jpra0204 <jp.roca.angulo@gmail.com>
2025-04-14 16:40:30 -0400

    creating the custom error page

--- app/error.jsx
+++ app/error.jsx
@@ -1,19 +1,21 @@
+'use client';
+
 import React from 'react'
 import Link from 'next/link';
-import { FaExclamationTriangle } from "react-icons/fa";
+import { FaExclamationCircle } from "react-icons/fa";
 
-const NotFoundPage = () => {
+const ErrorPage = ({ error }) => {
     return(
         <section className="bg-blue-50 min-h-screen flex-grow">
             <div className="container m-auto max-w-2xl py-24">
                 <div className="bg-white px-6 py-24 mb-4 shadow-md rounded-md border m-4 md:m-0">
                     <div className="flex justify-center">
-                        <FaExclamationTriangle className="fas fa-exclamation-triangle fa-5x text-8xl text-yellow-400" />
+                        <FaExclamationCircle className="fas fa-exclamation-circle fa-5x text-8xl text-yellow-400" />
                     </div>
                     <div className="text-center">
-                        <h1 className="text-3xl font-bold mt-4 mb-2">Page Not Found</h1>
+                        <h1 className="text-3xl font-bold mt-4 mb-2">Oups! Something went wrong!</h1>
                         <p className="text-gray-500 text-xl mb-10">
-                            The page you are looking for does not exist.
+                            {error.toString()}
                         </p>
                         <Link href="/" className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-4 px-6 rounded">
                             Go Home
@@ -26,4 +28,4 @@ const NotFoundPage = () => {
     )
 }
 
-export default NotFoundPage;
+export default ErrorPage;
